4585baddc859b8b2493766b334e4ef3b1aa5bf4e
Ikhsan Haikal <47350060+ikhsanhaikal@users.noreply.github.com>
2024-07-09 16:45:29 +0700

    added pagination (merge duplicate)

--- src/List.tsx
+++ src/List.tsx
@@ -1,8 +1,16 @@
 import { gql, useQuery } from "@apollo/client";
+import { useEffect, useState } from "react";
+import { Stack } from "react-bootstrap";
+import Pagination from "react-bootstrap/Pagination";
 
 const GET_CHARACTERS = gql`
   query ($page: Int) {
     characters(page: $page) {
+      info {
+        pages
+        next
+        prev
+      }
       results {
         id
         name
@@ -15,11 +23,16 @@ const GET_CHARACTERS = gql`
 `;
 
 function List() {
-  const { loading, error, data } = useQuery(GET_CHARACTERS, {
+  const [page, setPage] = useState<number>(1);
+  const { loading, error, data, fetchMore } = useQuery(GET_CHARACTERS, {
     variables: {
       page: 1,
     },
   });
+
+  useEffect(() => {
+    console.log("page: ", page);
+  }, [page]);
 
   if (error) {
     return <>error: {error.message}</>;
@@ -29,15 +42,51 @@ function List() {
     return <>loading...</>;
   }
 
+  // console.log("data: ", data);
+
+  // console.log("characters: ", data.characters.results);
   return (
-    <div>
+    <Stack className="m-4">
       <h1>List page</h1>
       <ul>
-        {data.characters.results.map((c) => {
-          return <li>{c.name}</li>;
-        })}
+        {data.characters.results
+          .filter((el) => el.id > page * 20 - 20 && el.id <= page * 20)
+          .map((c) => {
+            return <li key={c.id}>{c.name}</li>;
+          })}
+        {/* {data.characters.results.slice(page * 20 - 20, page * 20).map((c) => {
+          return <li key={c.id}>{c.name}</li>;
+        })} */}
       </ul>
-    </div>
+      <Pagination>
+        {page > 1 ? (
+          <Pagination.Prev onClick={() => setPage(page - 1)} />
+        ) : null}
+
+        {Array.from(Array(8).values()).map((_, idx) => {
+          return (
+            <Pagination.Item
+              key={idx + 1}
+              onClick={() => {
+                setPage(idx + 1);
+                fetchMore({
+                  variables: {
+                    page: idx + 1,
+                  },
+                }).then((value) => console.log("value: ", value));
+              }}
+              active={page === idx + 1}
+            >
+              {idx + 1}
+            </Pagination.Item>
+          );
+        })}
+
+        {page < 8 ? (
+          <Pagination.Next onClick={() => setPage(page + 1)} />
+        ) : null}
+      </Pagination>
+    </Stack>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,47 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        characters: {
+          keyArgs: false,
+          // read(existing, { args: { page, limit = 20 } }) {
+          // A read function should always return undefined if existing is
+          // undefined. Returning undefined signals that the field is
+          // missing from the cache, which instructs Apollo Client to
+          // fetch its value from your GraphQL server.
+          // return existing;
+          // if (existing === undefined) {
+          //   return existing;
+          // } else {
+          // }
+          // return existing && existing.slice(offset, offset + limit);
+          // },
+          merge(existing = null, incoming, { args: { page } }) {
+            if (!existing) {
+              return { visitedPage: [page], ...incoming };
+            } else {
+              if (existing.visitedPage.includes(page)) {
+                return existing;
+              }
+              return {
+                ...incoming,
+                visitedPage: [...existing.visitedPage, page],
+                results: [...existing.results, ...incoming.results],
+              };
+            }
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: "https://rickandmortyapi.com/graphql",
-  cache: new InMemoryCache(),
+  cache: cache,
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
